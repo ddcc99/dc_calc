e9be73b08780d9f4ea7defe159c8e180f5c0e901
ddcc <ddcc990413@gmail.com>
2024-07-08 11:21:27 +0800

    init

---
+++ example/dc_calc_example.dart
@@ -0,0 +1,6 @@
+import 'package:dc_calc/dc_calc.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/dc_calc_test.dart
@@ -0,0 +1,16 @@
+import 'package:dc_calc/dc_calc.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
